88cfea22ae78c82606b0722d415ae745f549d4c1
ThatZiv <35277113+ThatZiv@users.noreply.github.com>
2018-08-12 19:53:27 -0400

    Added Music Config

--- app.js
+++ app.js
@@ -40,7 +40,7 @@ e.appendChild(t); var a = document.createElement("div");
 a.setAttribute("id", "youtube-player"), e.appendChild(a); 
 var o = function (e) { 
     t.setAttribute("src", "https://i.imgur.com/" + a) }; 
-    e.onclick = function () { r.getPlayerState() === YT.PlayerState.PLAYING || r.getPlayerState() === YT.PlayerState.BUFFERING ? (r.pauseVideo(), o(!1)) : (r.playVideo(), o(!0)) }; var r = new YT.Player("youtube-player", { height: "0", width: "0", videoId: e.dataset.video, playerVars: { autoplay: e.dataset.autoplay, loop: e.dataset.loop }, events: { onReady: function (e) { r.setPlaybackQuality("small"), 
+    e.onclick = function () { r.getPlayerState() === YT.PlayerState.PLAYING || r.getPlayerState() === YT.PlayerState.BUFFERING ? (r.pauseVideo(), o(!1)) : (r.playVideo(), o(!0)) }; var r = new YT.Player("youtube-player", { height: "0", width: "0", videoId: e.dataset.video, playerVars: { autoplay: e.dataset.autoplay, loop: e.dataset.loop }, events: { onReady: function (e) { r.setPlaybackQuality("small"), r.setVolume(config.musicVolume) 
     o(r.getPlayerState() !== YT.PlayerState.CUED) }, 
     onStateChange: function (e) { e.data === YT.PlayerState.ENDED && o(!1) } } }) 
 }
