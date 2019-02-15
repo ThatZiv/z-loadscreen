7ee5ffc17be9d17dfd4e1a90ecb19cb487ffd0e3
Noah <10456231+How-Bout-No@users.noreply.github.com>
2019-02-15 13:53:33 -0500

    Update config.js

--- config.js
+++ config.js
@@ -5,12 +5,13 @@ var config = {
     "imgInterval": 8000,
     "music": true,
     /* Insert the Youtube Video ID below */
-    "videoID": "jo4-FhqkNwQ",
+    //"videoID": "poKqspDfElw",
+    "videoID": "-uiv4zMtkxI",
     /* Music Volume (0%-100%) | Lower is Quieter, Higher is Louder */
-    "musicVolume": 70,
+    "musicVolume": 25,
     "text": {
-        "title": "My Arpee Server",
-        "link": "discord.gg/yeet",
+        "title": "Header",
+		"text": "This is some explanation text showing border control of the screen<br><br><u>All HTML Elements should be supported;</u><br><br><li>Lists</li><br><i><b>Styled</b> <u>Text</u></i><br><br>Even images, if you want<br><img src='./imgs/bg1.png' style='width: 250px;'>",
     },
     /* You dont have to include the '/imgs/' dir AND ALSO REMEMBER TO ADD THESE IMGS IN YOUR __resource.lua*/ 
     "images": ['bg0.png', 'bg1.png', 'bg2.png', 'bg3.png', 'bg4.png', 'bg5.png', 'bg6.png', 'bg7.png']
